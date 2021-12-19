8fe3305498321207e41934f267039e37eaaa947d
yihuier <yihui_mail@foxmail.com>
2021-12-19 12:29:45 +0800

    init

---
+++ src/main/java/com/example/jenkinsdemo/JenkinsDemoApplication.java
@@ -0,0 +1,20 @@
+package com.example.jenkinsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class JenkinsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsDemoApplication.class, args);
+    }
+
+    @RequestMapping("/demo")
+    public String demo() {
+        return "hello jenkins";
+    }
+}
